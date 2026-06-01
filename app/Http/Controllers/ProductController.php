c4f08a8dac66a46835b70e23baca2100d1591d85
fabianasore <fabianasore@gmail.com>
2026-06-01 00:10:48 -0300

    feat: integra home com tabela de produtos e exibe produtos cadastrados

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,20 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produto;
 use Illuminate\Http\Request;
 // use App\Models\Product;
 
 class ProductController extends Controller
 {
-    public function home(){
-        return view('home');
-    }
+    public function home()
+    {
+        $categorias = Produto::select('categoria')
+        ->distinct()
+        ->pluck('categoria');
+
+        $produtosDestaque = Produto::take(4)->get();
+        $outrosProdutos = Produto::skip(4)->take(8)->get();
+
+        return view('home', compact(
+        'categorias',
+        'produtosDestaque',
+        'outrosProdutos'
+));
+
+}
 
     public function index(){
         return view('products.index');
     }
 
-    public function show($id){
-        return view('products.show', compact('id'));
+    public function show($id)
+    {
+        $produto = Produto::findOrFail($id);
+        return view('products.show', compact('produto'));
     }
 }
